27ac254c93b94567acdb91db965e490142535d5b
jumang4423 <catontheskate4423@gmail.com>
2024-04-29 00:31:21 +0900

    123

--- v1/main.py
+++ v1/main.py
@@ -1,16 +1,22 @@
 
 import pygame
 import sys
+import random
 from pygame.locals import *
 from pydub import AudioSegment
 from pydub.playback import play
 import serial
 
 ## serial config ##
-DEVICE_NAME = '/dev/ttyACM0'
-if sys.argv[1] == 'windows':
-    DEVICE_NAME = 'COM3'
-serial_port =  serial.Serial(DEVICE_NAME, 9600),
+DEVICE_NAME = ''
+serial_port = None
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'windows':
+        DEVICE_NAME = 'COM3'
+    if sys.argv[1] == 'mac':
+        DEVICE_NAME = '/dev/tty.ACM0'
+if DEVICE_NAME != '':
+    serial_port =  serial.Serial(DEVICE_NAME, 9600)
 
 
 def terminate():
@@ -20,6 +26,9 @@ def terminate():
     
 
 # init pygame
+pad_arr_size = 12
+block_size = 100
+max_col = 6
 audioSettings = {"frequency": 44100, "size": -16, "channels": 2, "buffer": 256}
 system_fps = 120.0
 pygame.mixer.pre_init(
@@ -33,78 +42,66 @@ pygame.init()
 pygame.display.set_caption("cj_v74cl_hacks v1")
 main_clock = pygame.time.Clock()
 window_surface = pygame.display.set_mode(
-    (640, 480)
+    (block_size * max_col, block_size * (pad_arr_size // max_col) + 50)
 )
 
 # vars 2
-font = pygame.font.SysFont(None, 23)
+font = pygame.font.SysFont(None, 30)
 bg_color = (255, 255, 255)
 text_color = (10, 10, 10)
-s_ki = pygame.mixer.Sound("sounds/ki.wav")
-s_rim = pygame.mixer.Sound("sounds/rim.wav")
-s_hi = pygame.mixer.Sound("sounds/hi.wav")
-s_m = [pygame.mixer.Sound(f"sounds/m{i}.wav") for i in range(1, 6)]
-pad_colors = {
-    "ki": (255, 0, 0),
-    "hi": (0, 255, 0),
-    "rim": (0, 0, 255),
-    "m": [
-        (255, 255, 0),
-        (255, 0, 255),
-        (0, 255, 255),
-        (100, 0, 255),
-        (0, 0, 0)
-    ]
-}
+# 0,1,2,3,4,5,6,7,8,9,*,#
+def load_sounds():
+    arr = []
+    for i in range(pad_arr_size):
+        # check if file exists
+        try:
+            s = pygame.mixer.Sound(f"sounds/{i}.wav")
+            arr.append(s)
+        except:
+            arr.append(None)
+            print(f"Error: sounds/m{i}.wav not found")
+    return arr
+
+s_m = load_sounds()
 pad_value_max = 255
-pad_values = {
-    "ki": pad_value_max,
-    "hi": pad_value_max,
-    "rim": pad_value_max,
-    "m": [pad_value_max, pad_value_max, pad_value_max, pad_value_max, pad_value_max]   
-}
+pad_values = [pad_value_max for i in range(pad_arr_size)]
 
 def on_parse_keys(event, que):
     if event.key == K_1:
-        que["m"]["1"] = True
+        que[1] = True
     if event.key == K_2:
-        que["m"]["2"] = True
+        que[2] = True
     if event.key == K_3:
-        que["m"]["3"] = True
+        que[3] = True
     if event.key == K_4:
-        que["m"]["4"] = True
+        que[4] = True
     if event.key == K_5:
-        que["m"]["5"] = True
+        que[5] = True
+    if event.key == K_6:
+        que[6] = True
+    if event.key == K_7:
+        que[7] = True
+    if event.key == K_8:
+        que[8] = True
+    if event.key == K_9:
+        que[9] = True
     if event.key == K_a:
-        que["ki"] = True
+        que[10] = True
     if event.key == K_s:
-        que["hi"] = True
+        que[0] = True
     if event.key == K_d:
-        que["rim"] = True
+        que[11] = True
     return que
 
 def player(que):
-    if que["ki"]:
-        s_ki.play()
-    if que["hi"]:
-        s_hi.play()
-    if que["rim"]:
-        s_rim.play()
-    for i in range(1, 6):
-        if que["m"][str(i)]:
-            s_m[i-1].play()
+    for i in range(pad_arr_size):
+        if que[i] and s_m[i]:
+            s_m[i].play()
 
 def update_value(que):
-    # if key pressed, set value to max. each frame, reduce value by 1
-    if que["ki"]:
-        pad_values["ki"] = pad_value_max
-    if que["hi"]:
-        pad_values["hi"] = pad_value_max
-    if que["rim"]:
-        pad_values["rim"] = pad_value_max
-    for i in range(1, 6):
-        if que["m"][str(i)]:
-            pad_values["m"][i-1] = pad_value_max
+    for i in range(pad_arr_size):
+        if que[i] and s_m[i]:
+            pad_values[i] = pad_value_max
 
 
 def draw_rect_alpha(surface, color, rect):
@@ -116,51 +113,33 @@ def gen_rbga(color, alpha):
     return (color[0], color[1], color[2], alpha)
 
 def update_ui():
-    block_size = 50
-    draw_rect_alpha(window_surface, gen_rbga(pad_colors["ki"], pad_values["ki"]), (0, 0, block_size, block_size))
-    draw_rect_alpha(window_surface, gen_rbga(pad_colors["hi"], pad_values["hi"]), (block_size, 0, block_size, block_size))
-    draw_rect_alpha(window_surface, gen_rbga(pad_colors["rim"], pad_values["rim"]), (block_size*2, 0, block_size, block_size))
-    for i in range(0, 5):
-        draw_rect_alpha(window_surface, gen_rbga(pad_colors["m"][i], pad_values["m"][i]), (block_size*(i), block_size, block_size, block_size))
-    
+    for i in range(pad_arr_size // max_col):
+        for j in range(max_col):
+            idx = i * max_col + j
+            draw_rect_alpha(window_surface, gen_rbga(text_color, pad_values[idx]), (j * block_size, i * block_size, block_size, block_size))
         
     # reduce value by n
-    n = 3
-    for i in range(1, 6):
-        pad_values["m"][i-1] = pad_values["m"][i-1] - n
-        if pad_values["m"][i-1] < 0:
-            pad_values["m"][i-1] = 0
-    pad_values["ki"] = pad_values["ki"] - n
-    if pad_values["ki"] < 0:
-        pad_values["ki"] = 0
-    pad_values["hi"] = pad_values["hi"] - n
-    if pad_values["hi"] < 0:
-        pad_values["hi"] = 0
-    pad_values["rim"] = pad_values["rim"] - n
-    if pad_values["rim"] < 0:
-        pad_values["rim"] = 0
-def read_serial(que):
+    n = 8
+    for i in range(pad_arr_size):
+        pad_values[i] = pad_values[i] - n
+        if pad_values[i] < 0:
+            pad_values[i] = 0
 
-
-               
+last_serial_data = [0 for i in range(pad_arr_size)]
+def update_serial_data(que):
+    now_serial_data = [int(i) for i in serial_port.readline().decode().strip().split(',')]
+    for i in range(pad_arr_size):
+        if now_serial_data[i] == 1 and last_serial_data[i] == 0:
+            que[i] = True
+        last_serial_data[i] = now_serial_data[i]
+        
 
 while True:
     window_surface.fill(bg_color)
-    text = font.render("Press 1-5, a, s, d to play sound", True, text_color)
-    window_surface.blit(text, (10, 120))
+    text = font.render("Press 1-9, a, s, d to play sound", False, text_color, bg_color)
+    window_surface.blit(text, (10, block_size * (pad_arr_size // max_col) + 15))
 
-    que = {
-        "ki": False,
-        "hi": False,
-        "rim": False,
-        "m": {
-            "1": False,
-            "2": False,
-            "3": False,
-            "4": False,
-            "5": False,
-        }
-    }
+    que = [False for i in range(pad_arr_size)]
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -171,14 +150,10 @@ while True:
         if event.type == KEYDOWN:
             que = on_parse_keys(event, que)
                 
-        # from arduino
-        
-
-    # update ui based on que
     update_value(que)
-    # player
+    if serial_port:
+        update_serial_data()
     player(que)
-
     update_ui()
 
     pygame.display.update()
